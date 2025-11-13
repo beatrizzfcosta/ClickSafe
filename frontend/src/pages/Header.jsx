cc93db1a8a396c941c389a5cfc43f79133bf223d
nadiaprg <nprga@iscte-iul.pt>
2025-11-13 21:53:09 +0000

    criação do frontend
    Criei o frontend com React + Vite.
    
    Comecei a fazer a Home page, com o footer e o header quase concluídos.

---
+++ frontend/src/pages/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+
+    return (
+        <>
+            <header>
+                <div className="header">
+                    <h1>ClickSafe</h1>
+                    <nav className="nav">
+                        <input type="text" placeholder="Enter website or URL here"/>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx';
+import Footer from "./Footer.jsx";
+function Home() {
+
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
