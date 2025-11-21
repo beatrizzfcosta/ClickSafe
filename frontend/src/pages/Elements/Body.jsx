1d975803fc44c9c60f8f9cced332e0e678e34975
nadiaprg <nprga@iscte-iul.pt>
2025-11-21 20:45:18 +0000

    update ao frontend
    Adicionei o que tinha no outro branch que se aproveitava

--- frontend/src/pages/Elements/Body.jsx
+++ frontend/src/pages/Elements/Body.jsx
@@ -1,41 +1,57 @@
-import Resposta from "./Resposta.jsx";
 import {useState} from "react";
 function Body(){
+
     const [url, setURL] = useState('')
+    const [response, setResponse] = useState(null)
 
-    const submit=async(e)=>{
+    const submit= async (e)=>{
         e.preventDefault()
-    }
 
-    async function get_link_id(URL, URL_normalizado){
-        const get_link_id_BD = ''
-
-        try {
-
-        } catch (error) {
-
+        if (!url){
+            setResponse({error: "Insira uma URL válida"})
+            return
         }
+
+        setResponse(null)
+
+        try{
+            const result = app.py [url]
+
+            setResponse(result)
+        } catch (e){
+            setResponse({error: "Problema no servidor"})
+        }
+
     }
 
     return (
         <>
             <main id="body">
                 <div className="URL">
-                    <input type="text" onChange={(e) => {setURL(e.target.value)}} onClick={submit} placeholder="Enter website or URL here"/>
+                    <form>
+                        <input type="text" value={url} onChange={(e) => {
+                            setURL(e.target.value)
+                        }} placeholder="Enter website or URL here"/>
+
+                        <button type={submit}>
+                            submit
+                        </button>
+                    </form>
+
                 </div>
                 <div className="resposta">
-                    <p>
-                        Devido ao aumento de ataques de phishing nos últimos anos,
-                        ataques que têm como objetivo enganar os utilizadores de
-                        forma a que os mesmos revelem dados sensiveis ou cliquem em links maliciosos,
-                        considerámos necessários desenvolver uma aplicação que ajuda-se a preveni-los.
-                    </p>
-                    <Resposta/>
+                    <Resposta response={response}/>
                 </div>
 
             </main>
         </>
     )
 }
+
+const Resposta = ({response}) => (
+    <div>
+        <p>{response}</p>
+    </div>
+);
 
 export default Body
